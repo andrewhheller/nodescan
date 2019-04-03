0a47a60f1326aebfabee8a8386037deeeda47496
Andrew Heller <andrewhheller@gmail.com>
2019-04-02 22:46:07 -0400

    add initial code to determine if node is installed.

--- nodescan.sh
+++ nodescan.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 echo "Welcome to nodeScan!"
+
+# checks if node is installed on system
+
+echo "Checking if node is installed on system... "
+
+if [[ $(which adfadfadf) ]]; then
+  echo 'Confirmed, node is installed. Scan proceeding.'
+else
+  echo 'Please note node is NOT installed on this system.  Nothing to search.  Script exiting.'
+fi

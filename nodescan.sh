d85e1e8678dfeed3a1fd854c7a5dc75f52813906
Andrew Heller <andrewhheller@gmail.com>
2019-04-06 19:43:19 -0400

    added find and format code to script file.

--- nodescan.sh
+++ nodescan.sh
@@ -5,8 +5,10 @@ echo "Welcome to nodeScan!"
 
 echo "Checking if node is installed on system... "
 
-if [[ $(which adfadfadf) ]]; then
-  echo 'Confirmed, node is installed. Scan proceeding.'
+if [[ $(which node) ]]; then
+  echo "Node found.  Executing script..."
+  echo "Results will be saved in log.txt of the current working directory."
+  find / -name node_modules > log.txt | awk -F "node_modules" '{print $1}' > log.txt | uniq -d log.txt
 else
-  echo 'Please note node is NOT installed on this system.  Nothing to search.  Script exiting.'
+  echo 'Node NOT found on this system.  Aborting.'
 fi

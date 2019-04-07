3849e2689936bf512eb20788f2ab070f29fbfb4a
Andrew Heller <andrewhheller@gmail.com>
2019-04-07 14:56:28 -0400

    added sort for duplicate removal

--- nodescan.sh
+++ nodescan.sh
@@ -3,16 +3,15 @@ echo "Welcome to nodeScan!"
 
 # first checks if node is installed on system
 
-echo "Checking if node is installed on system... "
-
 if [[ $(which node) ]]; then
-  echo "Node found.  Executing script..."
-  sudo mkdir /var/log/nodescan
+  echo "Node installed. Search for node applications..."
+  sudo mkdir -p /var/log/nodescan
   sudo chown $USER /var/log/nodescan
   touch /var/log/nodescan/log.txt
   touch /var/log/nodescan/error.txt
-  find / -name node_modules 2> /var/log/nodescan/error.txt | awk -F "node_modules" '{print $1}' | uniq -d > /var/log/nodescan/log.txt
-  echo "Scan complete.  Please open /var/log/nodeScan.txt to review results."
+  find / -name node_modules 2> /var/log/nodescan/error.txt | awk -F "node_modules" '{print $1}' > /var/log/nodescan/log.txt
+  sort -uo /var/log/nodescan/log.txt /var/log/nodescan/log.txt
+  echo "Scan complete.  Please open /var/log/nodescan/log.txt to review results."
 else
   echo 'Node NOT found on this system.  Aborting.'
 fi

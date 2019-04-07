bbb0ec87c05eaa52a7441b009400a89d980be89b
Andrew Heller <andrewhheller@gmail.com>
2019-04-06 21:37:40 -0400

    modified script file for permissions

--- nodescan.sh
+++ nodescan.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "Welcome to nodeScan!"
 
-# checks if node is installed on system
+# first checks if node is installed on system
 
 echo "Checking if node is installed on system... "
 
 if [[ $(which node) ]]; then
   echo "Node found.  Executing script..."
-  echo "Results will be saved in log.txt of the current working directory."
-  find / -name node_modules > log.txt | awk -F "node_modules" '{print $1}' > log.txt | uniq -d log.txt
+  touch /var/log/nodeScan.txt
+  chmod +w /var/log/nodeScan.txt
+  find /home/andrew/Desktop/FSA/flexImmersive/07_Express/code_express/ -name node_modules > /var/log/nodeScan.txt
+  awk -F "node_modules" '{print $1}' /var/log/nodeScan.txt | uniq -d > /var/log/nodeScan.txt
+  echo "Scan complete.  Please open /var/log/nodeScan.txt to review results."
 else
   echo 'Node NOT found on this system.  Aborting.'
 fi
